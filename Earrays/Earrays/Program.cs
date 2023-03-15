fa188f9a060c61038c8153f597ab7f59983c675b
YayisQuiroz <126173181+YayisQuiroz@users.noreply.github.com>
2023-03-14 19:46:40 -0600

    Add files via upload

---
+++ Earrays/Earrays/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Earrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaración de un arreglo de cadenas
+            string[] nombres = new string[] { "Juan", "María", "Pedro", "Luisa" };
+
+            // Imprimir el arreglo en la consola
+            Console.WriteLine("Los nombres en el arreglo son:");
+            for (int i = 0; i < nombres.Length; i++)
+            {
+                Console.WriteLine(nombres[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
